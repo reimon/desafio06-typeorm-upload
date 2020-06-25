8e3579f64d9b2e6034f17f8c9193ea12777c20f2
Reimon Ferreira <reimonf@gmail.com>
2020-06-25 08:48:01 -0300

    :rocket: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -12,7 +12,9 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -7,6 +7,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+
 import Category from './Category';
 
 @Entity('transactions')
@@ -23,7 +24,7 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
